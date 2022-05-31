103f0a2b7827a6217d03dd4011c422bda9ae8fd6
Anton Kondrašev <anton.kondrasev@linnworks.com>
2022-05-31 13:18:33 +0300

    comments

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+/*
+ * this is the main placeholder file which is going to register itself to linnworks apps at runtime
+ */
+
 import { ModalName } from "@linnworks/extension-sdk";
 
 //user import type instead of usual import to make sure it is not included in runtime bundle
@@ -6,6 +10,9 @@ import type { PermissionsUser } from "@linnworks/entities";
 //get registry from global scope, it has to be implemented by linnworks
 const registry = LinnworksApps.appRegistry;
 
+/*
+ * you can register placeholder under strictly defined key, if you try to make mistake - typescript will not compile
+ */
 registry.registerPlaceholderItem("user-management-user-list-buttons", {
     type: "button",
     icon: "fa fa-cog",
