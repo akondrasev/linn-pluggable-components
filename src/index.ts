b32eb7d87b1999ea3b037f5fa8f4df4a1f760e96
Anton Kondrašev <anton.kondrasev@linnworks.com>
2022-05-31 09:45:42 +0300

    trial cod e

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import { ModalName } from "@linnworks/extension-sdk";
+import { PermissionsUser } from "@linnworks/entities";
+
+const registry = LinnworksApps.appRegistry;
+
+registry.registerPlaceholderItem("user-management-user-list-buttons", {
+    type: "button",
+    icon: "fa fa-cog",
+    text: "Placeholder layer",
+    onClick(e, placeholder, proxy) {
+        const proxyDialog = proxy.openModal<never, PermissionsUser>({
+            name: ModalName.ADD_NEW_USER
+        });
+
+        if (proxyDialog === null) {
+            return;
+        }
+
+        proxyDialog.afterClosed.then(_ => {
+            console.log(_);
+        })
+    }
+})
