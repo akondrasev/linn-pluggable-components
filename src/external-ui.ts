950e49ac201dccd9461585d74782ac363663da46
Anton Kondrašev <anton.kondrasev@linnworks.com>
2022-06-15 14:00:40 +0300

    uncomment code

--- src/external-ui.ts
+++ src/external-ui.ts
@@ -1,9 +1,9 @@
 const registry = LinnworksApps.appRegistry;
 
-// registry.registerExternalUi({
-//     applicationUrl: "https://dashboard.xsellco.com/linnworks/auth/[{TOKEN}]",
-//     stateUrl: "xcello-app",
-//     leftMenuName: "XCELLO",
-//     leftMenuSection: "Inventory",
-//     title: "Xcello external ui"
-// });
+registry.registerExternalUi({
+    applicationUrl: "https://dashboard.xsellco.com/linnworks/auth/[{TOKEN}]",
+    stateUrl: "xcello-app",
+    leftMenuName: "XCELLO",
+    leftMenuSection: "Inventory",
+    title: "Xcello external ui"
+});
