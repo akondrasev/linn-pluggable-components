a38401daa2f4e6e994f26c7652110215cec42b08
Anton Kondrašev <anton.kondrasev@linnworks.com>
2022-06-16 09:51:27 +0300

    devtools: false

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
     entry: './src/index.ts',
+    devtool: false,
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: "auto",
