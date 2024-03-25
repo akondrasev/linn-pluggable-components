d728ba0abc0a941cd2658988b12b9b1a04bc3ad5
Anton Kondrašev <anton.kondrasev@linnworks.com>
2024-03-25 15:33:04 +0200

    ui-views implementation

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,75 @@
 const registry = LinnworksApps.appRegistry;
 
-registry.registerPlaceholderItem("user-management-user-list-buttons", {
-    type: "button",
-    icon: "fa fa-plus",
-    text: "Example app button",
+let um = registry.view('user-management-users');
+let inventory = registry.view('my-inventory');
+let po = registry.view('process-orders-popup');
+
+um.whenReady((state) => {
+    console.log("ready: ", state);
+})
+
+um.whenStateUpdated((state) => {
+    console.log("state updated", state);
+});
+
+um.addButton({
+    text: "New generation",
+    icon: "fa fa-print",
     onClick(e, placeholder, proxy) {
-        console.log(proxy.uiService.session);
+        console.log(proxy.uiService.viewState);
 
-        proxy.uiService.getSelectedPageDataItems().then(_ => {
-            console.log(_);
-        });
+        // proxy.uiService.openModal({
+        //     name: "ADD_NEW_USER"
+        // })
+    },
+    place: 'toolbar-buttons'
+});
 
-        const proxyDialog = proxy.uiService.openExtensionDialog("DIALOG_REF", {
-            dialogTitle: "Title",
-            dialogActions: [],
-            data: {
-                test: "string"
-            }//pass anything to your dialog, this data should be passed inside iframe by linnworks host
-        });
 
-        if (proxyDialog === null) {
-            return;
-        }
+inventory.whenReady((state) => {
+    console.log("inventory ready", state);
+})
 
-        proxyDialog.afterClosed.then(_ => {
-            console.log(_);
-        });
+inventory.whenStateUpdated((state) => {
+    console.log('inventory state update', state)
+})
+
+inventory.addButton({
+    text: "FFFF",
+    place: "toolbar-buttons",
+    onClick(...{2: layer}) {
+        console.log('inventory click', layer.uiService.viewState);
     }
-});
+})
 
-registry.registerExternalDialog({
-    dialogUrl: "http://localhost:8080/",
-    name: "DIALOG_REF"
-});
 
-registry.registerExternalUi({
-    applicationUrl: "https://dashboard.xsellco.com/linnworks/auth/[{TOKEN}]",
-    stateUrl: "xcello-app",
-    leftMenuName: "XCELLO",
-    leftMenuSection: "Inventory",
-    title: "Xcello external ui"
-});
+po.whenReady((state) => {
+    console.log("po ready", state)
+})
+
+po.whenStateUpdated((state) => {
+    console.log('po state update', state)
+})
+
+po.addButton({
+    text: "FFFF",
+    place: "popup-action-buttons",
+    onClick(...{2: layer}) {
+        console.log('po click', layer.uiService.viewState)
+    }
+})
+
+
+//
+// registry.registerExternalDialog({
+//     dialogUrl: "http://localhost:8080/",
+//     name: "DIALOG_REF"
+// });
+//
+// registry.registerExternalUi({
+//     applicationUrl: "https://dashboard.xsellco.com/linnworks/auth/[{TOKEN}]",
+//     stateUrl: "xcello-app",
+//     leftMenuName: "XCELLO",
+//     leftMenuSection: "Inventory",
+//     title: "Xcello external ui"
+// });
