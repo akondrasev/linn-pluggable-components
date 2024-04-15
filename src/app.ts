92b95505521e07c94cb18f394c1e0b1f4df2a4f4
Anton Kondrašev <anton.kondrasev@linnworks.com>
2024-04-15 16:04:13 +0300

    editor config

--- src/app.ts
+++ src/app.ts
@@ -2,75 +2,92 @@ const registry = LinnworksApps.appRegistry;
 
 
 {
-    let um = registry.view('user-management-users');
+  let um = registry.view('user-management-users');
 
-    um.whenReady((state) => {
-        console.log("ready: ", state);
-    })
+  um.whenReady((state) => {
+    console.log("ready: ", state);
+  })
 
-    um.whenStateUpdated((state) => {
-        console.log("state updated", state);
-    });
+  um.whenStateUpdated((state) => {
+    console.log("state updated", state);
+  });
 
-    um.addButton({
-        text: "New generation",
-        icon: "fa fa-print",
-        onClick(e, placeholder, proxy) {
-            console.log(proxy.uiService.viewState);
+  um.addButton({
+    text: "New generation",
+    icon: "fa fa-print",
+    onClick(e, placeholder, proxy) {
+      console.log(proxy.uiService.viewState);
 
-            // proxy.uiService.openModal({
-            //     name: "ADD_NEW_USER"
-            // })
-        },
-        place: 'toolbar-buttons'
-    });
+      // proxy.uiService.openModal({
+      //     name: "ADD_NEW_USER"
+      // })
+    },
+    place: 'toolbar-buttons'
+  });
 }
 
 {
-    let inventory = registry.view('my-inventory');
+  let inventory = registry.view('my-inventory');
 
-    let disabled = true;
-    inventory.whenReady((state) => {
-        console.log("inventory ready", state);
-    })
+  let disabled = true;
+  inventory.whenReady((state) => {
+    console.log("inventory ready", state);
+  })
 
-    inventory.whenStateUpdated((state) => {
-        console.log('inventory state update', state)
+  inventory.whenStateUpdated((state) => {
+    console.log('inventory state update', state)
 
-        disabled = state.selectedItems.length === 0;
-    })
+    disabled = state.selectedItems.length === 0;
+  })
 
-    inventory.addButton({
-        text: "FFFF",
-        place: "toolbar-buttons",
-        onClick(...{2: layer}) {
-            console.log('inventory click', layer.uiService.viewState);
-        },
-        isDisabled() {
-            return disabled;
-        }
-    })
+  inventory.addButton({
+    text: "FFFF",
+    place: "toolbar-buttons",
+    onClick(...{2: layer}) {
+      console.log('inventory click', layer.uiService.viewState);
+    },
+    isDisabled() {
+      return disabled;
+    }
+  })
 }
 
 
 {
-    let po = registry.view('process-orders-popup');
+  let po = registry.view('process-orders-popup');
 
-    po.whenReady((state) => {
-        console.log("po ready", state)
-    })
+  po.whenReady((state) => {
+    console.log("po ready", state)
+  })
 
-    po.whenStateUpdated((state) => {
-        console.log('po state update', state)
-    })
+  po.whenStateUpdated((state) => {
+    console.log('po state update', state)
+  })
 
-    po.addButton({
-        text: "FFFF",
-        place: "popup-action-buttons",
-        onClick(...{2: layer}) {
-            console.log('po click', layer.uiService.viewState)
-        }
-    })
+  po.addButton({
+    text: "Fake scan",
+    place: "popup-action-buttons",
+    onClick(...{2: layer}) {
+      console.log('po click', layer.uiService.viewState)
+    }
+  })
+}
+
+
+{
+  let OO = registry.view('open-orders-new');
+
+  OO.whenStateUpdated((state) => {
+    console.log('OO state update', state)
+  })
+
+  OO.addButton({
+    text: "Pluggable",
+    place: "toolbar-buttons",
+    onClick(...{2: layer}) {
+      console.log('OO click', layer.uiService.viewState);
+    }
+  })
 }
 
 //
